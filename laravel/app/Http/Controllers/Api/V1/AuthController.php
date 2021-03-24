71d4c8df3cabce03b4115535962396aa9770890c
Rheza Sindhuwinata <rheza.s@gmail.com>
2021-03-24 09:29:04 +0700

    - Add .env.prod for laravel - install health check laravel - install debugbar laravel - api versioning - standarized api response format

---
+++ laravel/app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class AuthController extends Controller {
+
+    public $successStatus = 200;
+
+    public function login() {
+        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+            $user = Auth::user();
+
+            $result['data'] = [
+                'user' => $user,
+                'token' => $user->createToken('nApp')->accessToken
+            ];
+            return $this->trJsonSuccess($result, 200, 'Success Login');
+        } else {
+            return $this->trJsonError(401, 'Unauthorised');
+        }
+    }
+
+    public function register(UserRequest $request) {
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+
+        $result['data'] = [
+            'user' => $user,
+            'token' => $user->createToken('nApp')->accessToken
+        ];
+
+        return $this->trJsonSuccess($result, 200, 'Success Register');
+    }
+}
---
+++ laravel/routes/api_v1.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\InitController;
+use App\Http\Controllers\Api\V1\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group(['namespace' => 'Api\V1'], function(){
+    Route::get('init', [InitController::class, 'index']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+
+    Route::group(['middleware' => ['auth:api']], function() {
+        Route::get('profile', [UserController::class, 'profile']);
+    });
+});
