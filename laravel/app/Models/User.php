0af471539b634ea59dd5da320f7f5a754b7343bc
Rheza Sindhuwinata <rheza.s@gmail.com>
2021-05-02 06:13:37 +0700

    - Final nginx optimization - Laravel cacheable users

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use GeneaLabs\LaravelModelCaching\Traits\Cachable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -11,7 +12,7 @@ use Auth;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use Cachable, HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
